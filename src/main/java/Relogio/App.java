03ebc5ecacca99a3eb57b486ea10a090a7dcec49
Charles Fonseca <charlesjrfonseca@gmail.com>
2019-08-15 19:54:44 -0300

    Refatoração ajusteHora

--- src/main/java/Relogio/App.java
+++ src/main/java/Relogio/App.java
@@ -8,8 +8,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Relogio r = new Relogio();
-        r.ajusteHora(24, 61, 10);
         r.ajusteData(20, 12);
+        r.ajusteHora(23, 61, 10);
 
         r.exibeInfomacoes();
     }
--- src/main/java/Relogio/Relogio.java
+++ src/main/java/Relogio/Relogio.java
@@ -3,16 +3,72 @@ package Relogio;
 public class Relogio {
 	public int hora = 0, minuto = 0, segundo = 0;
 	public int dia = 1, mes = 1;
+	public static int contador = 0;
 
 	public void ajusteHora(int h, int m, int s) {
-		validaSeg(s);
-		validaMin(m);
-		validaHora(h);
+		// int cicloRestoMin = validaSeg(s);
+		// int cicloRestoHora = validaMin(m + validaSeg(s));
+		// int cicloRestoDia = validaHora(h + validaMin(m + validaSeg(s)));
+		// int cicloRestoMes = validaDia(validaHora(h + validaMin(m + validaSeg(s))) +
+		// this.dia);
+		/*
+		 * São semelhantes, o código abaixo é muito mais ilegível que o acima,no entanto
+		 * é menos verboso.
+		 */
+		validaMes(validaDia(validaHora(h + validaMin(m + validaSeg(s))) + this.dia) + this.mes);
 	}
 
 	public void ajusteData(int d, int m) {
-		validaDia(d);
-		validaMes(m);
+		int cicloRestoMes = validaDia(d);
+		validaMes(m + cicloRestoMes);
+	}
+
+	private int validaSeg(int s) {
+		contador = 0;
+		while (s > 60) {
+			contador++;
+			s -= 60;
+		}
+		this.segundo = s;
+		return contador;
+	}
+
+	private int validaMin(int m) {
+		contador = 0;
+		while (m > 60) {
+			contador++;
+			m -= 60;
+		}
+		this.minuto = m;
+		return contador;
+	}
+
+	private int validaHora(int h) {
+		contador = 0;
+		while (h >= 24) {
+			contador++;
+			h -= 24;
+		}
+		this.hora = h;
+		return contador;
+	}
+
+	private int validaDia(int d) {
+		contador = 0;
+		while (d > 30) {
+			contador++;
+			d -= 30;
+		}
+		this.dia = d;
+		return contador;
+	}
+
+	private void validaMes(int m) {
+		contador = 0;
+		this.mes = m;
+		while (m > 12) {
+			this.mes--;
+		}
 	}
 
 	public void reiniciar(int h, int m, int s) {
@@ -21,46 +77,6 @@ public class Relogio {
 		this.minuto = 0;
 		this.segundo = 0;
 		this.hora = 0;
-	}
-
-	private void validaSeg(int s) {
-		while (s > 60) {
-			this.minuto++;
-			s -= 60;
-		}
-		this.segundo = s;
-	}
-
-	private void validaMin(int m) {
-		while (m > 60) {
-			this.hora++;
-			m -= 60;
-		}
-		this.minuto = m;
-
-	}
-
-	private void validaHora(int h) {
-		while (h > 24) {
-			this.dia++;
-			h -= 24;
-		}
-		this.hora = h;
-	}
-
-	private void validaDia(int d) {
-		while (d > 30) {
-			this.mes++;
-			d -= 30;
-		}
-		this.dia = d;
-	}
-
-	private void validaMes(int m) {
-		this.mes = m;
-		while (m > 12) {
-			this.mes--;
-		}
 	}
 
 	public void exibeInfomacoes() {
