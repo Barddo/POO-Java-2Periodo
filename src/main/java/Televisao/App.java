120e691caacb8d4ea9ab3572cd96c94aec07d359
Charles Fonseca <charlesjrfonseca@gmail.com>
2019-08-15 23:47:01 -0300

    Entrada opções validada

--- src/main/java/Televisao/App.java
+++ src/main/java/Televisao/App.java
@@ -1,12 +1,26 @@
 package Televisao;
 
+import java.io.Console;
+
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        String escolha = "";
         System.out.println("Super Ultra Televisão 3000");
+        Televisao tv = new Televisao();
+    }
 
+    public static void menu() {
+        System.out.flush();
+        System.out.println("1 - Ligar");
+        System.out.println("2 - Desligar");
+        System.out.println("3 - Mudar o canal");
+        System.out.println("4 - Exibir o canal atual");
+        System.out.println("5 - Aumentar o volume");
+        System.out.println("6 - Aumentar o volume");
+        System.out.println("0 - Sair");
     }
 }
--- src/main/java/Televisao/Televisao.java
+++ src/main/java/Televisao/Televisao.java
@@ -9,37 +9,110 @@ public class Televisao {
 
     private int volume = 20;
     private int canal = 3;
+    private boolean estado = false;
 
     public Televisao() throws InterruptedException {
-        System.out.println("Ligando...");
-        Console.class.wait(2000);
         this.ligar();
     }
 
-    public void ligar() {
-        System.out.println("Televisão ligada");
+    public void interpreta() throws InterruptedException {
+        System.out.flush();
+        int opcao = validaEntrada();
+
+        switch (opcao) {
+        case 0:
+            System.exit(0);
+            break;
+        case 1:
+            this.ligar();
+            break;
+        case 2:
+            this.desligar();
+            break;
+        case 3:
+            this.trocaCanal();
+            break;
+        case 4:
+            this.exibeCanal();
+            break;
+        case 5:
+            this.aumentaVolume();
+            break;
+        case 6:
+            this.diminuiVolume();
+            break;
+        default:
+            break;
+        }
+    }
+
+    private int validaEntrada() {
+        String operacao = System.console().readLine();
+        int numOperador = 0;
+        try {
+            numOperador = Integer.parseInt(operacao);
+            if (numOperador > 6) {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            System.out.println("Entrada inválida");
+            operacao = System.console().readLine();
+        }
+        return numOperador;
+    }
+
+    public void ligar() throws InterruptedException {
+        if (!this.estado) {
+            System.out.println("Ligando...");
+            System.out.wait(2000);
+            this.estado = true;
+        } else {
+            System.out.println("Televisão já ligada");
+        }
     }
 
     public void desligar() throws InterruptedException {
-        System.out.println("Desligando...");
-        Console.class.wait(2000);
-        System.out.println("Televisão desligada");
+        if (this.estado) {
+            System.out.println("Desligando...");
+            System.out.wait(2000);
+            this.estado = false;
+            System.out.println("Televisão desligada");
+        } else {
+            System.out.println("Televisão já desligada");
+        }
     }
 
-    public void aumentaVolume(int v) {
-        this.volume += v;
+    public void aumentaVolume() {
+        this.volume += validaValor("Digite o volume: ", 0, 100);
     }
 
-    public void diminuiVolume(int v) {
-        this.volume -= v;
+    public void diminuiVolume() {
+        this.volume -= validaValor("Digite o volume: ", 0, 100);
     }
 
-    public void trocaCanal(int c) {
-        this.canal = c;
+    public void trocaCanal() {
+        this.canal = validaValor("Digite o canal: ", 2, 85);
     }
 
     public void exibeCanal() {
         System.out.println("Canal " + this.canal);
+    }
+
+    private int validaValor(String textoSaida, int menor, int maior) {
+        System.out.println(textoSaida);
+        String operacao = System.console().readLine();
+
+        int numero = 0;
+        try {
+            numero = Integer.parseInt(operacao);
+            if (numero < menor || numero > maior) {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            System.out.println("Entrada inválida");
+            operacao = System.console().readLine();
+        }
+        return numero;
     }
 
 }
