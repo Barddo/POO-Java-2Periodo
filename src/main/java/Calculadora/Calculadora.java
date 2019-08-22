a4c79c85023f55552d78b319402b8dbcfea32d74
Charles Fonseca <charlesjrfonseca@gmail.com>
2019-08-22 20:55:37 -0300

    Calculator updated

--- src/main/java/Calculadora/Calculadora.java
+++ src/main/java/Calculadora/Calculadora.java
@@ -1,5 +1,6 @@
 package Calculadora;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Calculadora {
@@ -17,13 +18,13 @@ public class Calculadora {
         return (elementoExiste && naoVazio);
     }
 
-    private String validaOperador(String textoSaida) {
+    private String validaOperador(String textoSaida) throws IOException {
         System.out.println(textoSaida);
-        String operacao = System.console().readLine();
+        String operacao = App.readLine();
         while (!EhValido(operacao)) {
             System.out.println("Entrada inválida");
             System.out.println(textoSaida);
-            operacao = System.console().readLine();
+            operacao = App.readLine();
         }
         return operacao;
     }
@@ -33,7 +34,24 @@ public class Calculadora {
         this.segundoNumero = valor2;
     }
 
-    public void RealizaOperacao() {
+    public int readInt(String textoSaida) throws IOException{
+        System.out.println(textoSaida);
+        String operacao;
+        int numero = 0;
+        try {
+            operacao = App.readLine();
+            numero = Integer.parseInt(operacao);
+            if (numero < 0) {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            System.out.println("Entrada inválida, digite novamente:");
+            numero = this.readInt(textoSaida);
+        }
+        return numero;
+    }
+
+    public void RealizaOperacao() throws IOException {
         String operador = validaOperador(
                 "Entre com a operação + (adição), - (subtração), * (multipicação), / (divisão), ^ (expoente) :");
         switch (operador) {
@@ -61,8 +79,6 @@ public class Calculadora {
             resultado = 0;
             break;
         }
-        System.out.printf("Resultado: " + this.primeiroNumero, operador, this.segundoNumero, resultado);
-
+        System.out.println("Resultado: " + this.primeiroNumero + operador + this.segundoNumero+" = " + resultado);
     }
-
 }
